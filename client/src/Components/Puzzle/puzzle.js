3682ac4236c6b025cd2cbba1baa088f38814a21b
Victor Marage <victor.marage666@gmail.com>
2019-11-22 15:17:54 -0500

    layout

--- client/src/Components/Puzzle/puzzle.js
+++ client/src/Components/Puzzle/puzzle.js
@@ -6,44 +6,35 @@ class Puzzle extends React.Component {
     render() {
         return (
             <>
+
+            <h1>TITLE</h1>
                 <section className="puzzle">
-                    {/* done   */}
+            
                     <div className="puzzle-blockA">
-                        <img src=""></img>
 
                         <div className="puzzle-blockA__1">
                         
                         </div>
                         <div className="puzzle-blockA__2">
-                            <img src=""></img>
+                            <div className="puzzle-blockA__2-a">
+                                1
+                            </div>
+
+                            <div className="puzzle-blockA__2-b">
+                                2
+                            </div>
                         </div>
                     </div>
 
-                    {/* done */}
                     <div className="puzzle-blockB">
 
                         <div className="puzzle-blockAB__1">
-
-                            <div className="puzzle-blockAB__2-A">
-
-                                <div className="1a">
-                                    <div className="1a2"></div>
-                                    <div className="1a3"></div>
-                                </div>
-
-                                <div className="2b"></div>
-                            </div>
-
-                            <div className="B">
-                            
-                            </div>
-                        </div>
-
-                        <div className="puzzle-blockAB__2">
                         
-                            <div className="puzzle-blockAB__2-A"></div>
-                            <div className="puzzle-blockAB__2-B"></div>
                         </div>
+                    </div>
+
+                    <div classname="text-box">
+                        <p className="text-box__content">text text text text text</p>
                     </div>
            
                 </section>
